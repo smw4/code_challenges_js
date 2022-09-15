4e3a20c7fca00de6dd35e0617e9171932b7b4336
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-15 11:37:05 -0400

    push the first item of one array to a new array. Plus pseudocode cleanup.

--- reverseArray.js
+++ reverseArray.js
@@ -3,22 +3,27 @@
 //create function body
 const reverseArray = function (array) {
 	let first = array[0];
-	return first;
+
+	// Create a new array to send items.
+	const newArray = [];
+	// Try using .push() to send "first" to "newArray"
+	newArray.push(first);
+
+	return newArray; // return contents of newArray so that it can log to console when called below.
 };
 
+//*************************************
 // example of passing an array into a function and calling the first item in the array.
 const fruits = ['banana', 'melon', 'apple'];
 
 console.log(reverseArray(fruits));
+//**********************************
 
 //logic of the function:
 //    - pass an array into the new function.
 //    - take the first item in the array and place it at the end of a new array.
 //    - take the second item in the array and do the same.
 //    - take the third item in the array and do the same.
-
-//- Once the function reencounters an item it has already sorted, return the new array.
-//    - if the object already has a unique ID, do not move it. Just return the array.
 
 //Calling the function
 //    console.log(reverseArray())
