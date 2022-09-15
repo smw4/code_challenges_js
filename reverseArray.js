98769bcb502d8e60807bbcb928a417708be9ce30
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-15 12:05:42 -0400

    pushign to array without needing a new variable plus Neater comments.

--- reverseArray.js
+++ reverseArray.js
@@ -2,12 +2,10 @@
 
 //create function body
 const reverseArray = function (array) {
-	let first = array[0];
-
 	// Create a new array to send items.
 	const newArray = [];
 	// Try using .push() to send "first" to "newArray"
-	newArray.push(first);
+	newArray.push(array[0]);
 
 	return newArray; // return contents of newArray so that it can log to console when called below.
 };
@@ -17,7 +15,6 @@ const reverseArray = function (array) {
 const fruits = ['banana', 'melon', 'apple'];
 
 console.log(reverseArray(fruits));
-//**********************************
 
 //logic of the function:
 //    - pass an array into the new function.
@@ -28,4 +25,12 @@ console.log(reverseArray(fruits));
 //Calling the function
 //    console.log(reverseArray())
 
-//META NOTE: Forget implementing the "assign unique ID" functionality for now. Might not be necessary because the function will already know the length of the array and knows when it has already reached the end.
+//#region : Concerns
+/*Above, I have hard-coded a variable, 'first' to denote the first item of an array. I can hard code 'second', 'third', etc. but I do not know how many items there may be in an array. If someone passes an array into my function with a greater number of items than I have hardcoded variables, the function will trip up.
+// Assumption: I want to automatically assign each item in an array its own variable. How can I automatically create new variables to equal the number of items in an array?
+// Critique: Do I need to assign each item its own variable in order to push it to the new array? If I can push an item to the array without giving it its own variable name, then do I really need to solve the above problem?
+// Outcome: I am able to send items to an array without assigning a unique */
+//#endregion
+//#region : META NOTE
+//Forget implementing the "assign unique ID" functionality for now. Might not be necessary because the function will already know the length of the array and knows when it has already reached the end.
+//#endregion
