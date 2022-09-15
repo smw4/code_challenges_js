35c6cf4ccd1f0bbeec229828dabd5e9fe93032cf
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-15 11:27:30 -0400

    refining pseudocode and adding first lines of code

--- reverseArray.js
+++ reverseArray.js
@@ -1,12 +1,19 @@
 //Write a function, reverseArray(), that takes in an array as an argument and returns a new array with the elements in the reverse order.
 
 //create function body
-//insert correct array syntax into parameter of function.
+const reverseArray = function (array) {
+	let first = array[0];
+	return first;
+};
+
+// example of passing an array into a function and calling the first item in the array.
+const fruits = ['banana', 'melon', 'apple'];
+
+console.log(reverseArray(fruits));
 
 //logic of the function:
-//- display the array in original order.
-//- take the first item in the array,
-//    - check if it has a unique ID. Assign one if it does not have one, then place it at the end of a new array.
+//    - pass an array into the new function.
+//    - take the first item in the array and place it at the end of a new array.
 //    - take the second item in the array and do the same.
 //    - take the third item in the array and do the same.
 
@@ -15,3 +22,5 @@
 
 //Calling the function
 //    console.log(reverseArray())
+
+//META NOTE: Forget implementing the "assign unique ID" functionality for now. Might not be necessary because the function will already know the length of the array and knows when it has already reached the end.
