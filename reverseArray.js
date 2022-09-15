7e53bbaa459b7a372cbbfdd662345f5a8adb3cd6
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-15 11:13:50 -0400

    pseudocode v1

---
+++ reverseArray.js
@@ -0,0 +1,17 @@
+//Write a function, reverseArray(), that takes in an array as an argument and returns a new array with the elements in the reverse order.
+
+//create function body
+//insert correct array syntax into parameter of function.
+
+//logic of the function:
+//- display the array in original order.
+//- take the first item in the array,
+//    - check if it has a unique ID. Assign one if it does not have one, then place it at the end of a new array.
+//    - take the second item in the array and do the same.
+//    - take the third item in the array and do the same.
+
+//- Once the function reencounters an item it has already sorted, return the new array.
+//    - if the object already has a unique ID, do not move it. Just return the array.
+
+//Calling the function
+//    console.log(reverseArray())
